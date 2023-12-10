b505aebbf4b5fa8a2290468e74d99ddf7ead7103
Joel Guingla <joel.guingla7@gmail.com>
2023-12-10 17:30:05 -0500

    First commit

---
+++ src/main/java/Logica/Main.java
@@ -0,0 +1,11 @@
+package Logica;
+
+import vista.JFTraductor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFTraductor jftraductor = new JFTraductor();
+        jftraductor.setVisible(true);
+    }
+}
