b695001f0eb42d84014758d993704d4643b0c398
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2024-01-01 20:17:30 -0500

    Traductor Version2

---
+++ Version-2/src/main/java/Logica/Main.java
@@ -0,0 +1,11 @@
+package Logica;
+
+import Vista.JFTraductor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFTraductor jftraductor = new JFTraductor();
+        jftraductor.setVisible(true);
+    }
+}
